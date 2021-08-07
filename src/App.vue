2eed34cfa8f88c47dbbdf47e50d9ab5f8816dad4
Kasjful Kurniawan <kasful.bpp@gmail.com>
2021-08-07 21:25:20 +0800

    installing tailwind

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,24 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div id="nav" class="flex p-3 justify-center bg-green-500 shadow-md">
+    <router-link class="linkNavbar" to="/">Home</router-link>
+    <router-link class="linkNavbar" to="/about">About</router-link>
   </div>
   <router-view />
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+.linkNavbar {
+  /* eslint-disable-next-line */
+  @apply px-3 flex justify-center;
 }
 
 #nav {
-  padding: 30px;
-
   a {
     font-weight: bold;
-    color: #2c3e50;
+    color: #ffffff;
 
     &.router-link-exact-active {
-      color: #42b983;
+      color: #313131;
     }
   }
 }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,21 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
+  <div class="container">
+    <div class="w-full flex justify-center"><img alt="Vue logo" src="../assets/logo.png" class="" /></div>
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    {{ webName }}
   </div>
 </template>
 
 <script setup lang="ts">
-export { default as HelloWorld } from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import { ref } from "vue";
+import { Options } from "vue-class-component";
+import { default as HelloWorld } from "@/components/HelloWorld.vue";
+
+Options({
+  components: {
+    HelloWorld,
+  },
+});
+
+const webName = ref("Vue.js + TypeScript");
 </script>
