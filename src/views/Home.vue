cfbd166354ed0560bdca78ca65e004f86b5b9300
Kasjful Kurniawan <kasful.bpp@gmail.com>
2021-08-07 15:22:31 +0800

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  </div>
+</template>
+
+<script setup lang="ts">
+export { default as HelloWorld } from "@/components/HelloWorld.vue"; // @ is an alias to /src
+</script>
